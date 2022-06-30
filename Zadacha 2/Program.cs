f63e500897c2816fce1bcae8d33e8eb52367c074
Alexander <masalov.alex1996@gmail.com>
2022-07-01 02:04:01 +0500

    Написали программу по второй задаче

---
+++ Zadacha 2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое число");
+double num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double num2 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2)
+{
+    double max = num1;
+    Console.Write("Максимальное число");
+    Console.WriteLine(max);
+    double min = num2;
+    Console.Write("Минимальное число");
+    Console.WriteLine(min);
+}
+else
+{
+    double max = num2;
+    Console.Write("Максимальное число");
+    Console.WriteLine(max);
+    double min = num1;
+    Console.Write("Минимальное число");
+    Console.WriteLine(min);
+}
+
+
+
+
+
