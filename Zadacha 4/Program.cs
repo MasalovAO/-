7fedf62f5824e8d70479ec973d5cedd11240948c
Alexander <masalov.alex1996@gmail.com>
2022-07-01 19:13:15 +0500

    Написали программу по четвертой задаче

---
+++ Zadacha 4/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max1 = num1;
+
+if(max1<num2)
+{
+    int max2 = num2;
+    if(max2<num3)
+    {
+        int max3 = num3;
+       Console.Write("Максимальное число из трех ");
+       Console.WriteLine(max3); 
+    }
+    else
+    { 
+        Console.Write("Максимальное число из трех ");
+        Console.WriteLine(max2);
+
+    }
+   
+}
+else
+{  
+    if(max1<num3)
+    {
+        Console.Write("Максимальное число из трех");
+        Console.WriteLine(num3);
+    }    
+
+    else
+    {
+        Console.Write("Максимальное число из трех ");
+        Console.WriteLine(max1);
+    }
+
+}
+
