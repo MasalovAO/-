2c95337bd5399056eb35a5620647b7f861c5dbc7
Alexander <masalov.alex1996@gmail.com>
2022-07-02 09:10:26 +0500

    Написали программу по восьмой задаче

---
+++ Zadacha 8/Program.cs
@@ -0,0 +1,20 @@
+﻿int i = 1;
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа в диапозоне от 1 до N ");
+while (i<=N)
+{   int number = i/2;
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+
+    }
+    else
+    {
+        Console.WriteLine("   ");
+    }
+
+    i++;
+}
+
+
