baa8f4211579480593e366abd371551c4f4c1ea8
Alexander <masalov.alex1996@gmail.com>
2022-07-02 06:58:54 +0500

    Написали программу к 6 задаче

---
+++ Zadacha 6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+double number = Convert.ToInt32(Console.ReadLine());
+double integer = number/2;
+if(number % 2 == 0)
+{
+    Console.Write("Ваше число четное ");
+    
+}
+else
+{
+    Console.Write("Ваше число нечетное ");
+    
+}
